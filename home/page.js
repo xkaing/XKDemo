b1229d0e48a62dcdb6d70d9446ef5fcfc33b08fc
wxk <wxk.cn@icloud.com>
2024-12-01 22:52:18 +0800

    1

---
+++ home/page.js
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href=""
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>XKDemo</h2>
+          <p>I have an idea</p>
+        </a>
+        <a
+          href="https://lfcoffee.cn"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            LFCoffee <span>-&gt;</span>
+          </h2>
+          <p>LaiFeng mobile-end dev team coffee log</p>
+        </a>
+
+        <a
+          href=""
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>XKTrophies</h2>
+          <p>PlayStation trophies data visualization</p>
+        </a>
+
+        <a
+          href=""
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>XKGenealog</h2>
+          <p>My paternal family genealogy book</p>
+        </a>
+      </div>
+    </main>
+  );
+}
